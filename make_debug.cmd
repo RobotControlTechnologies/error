84c74d8e149522c4e006179a2eae33d8b3df6485
Batykov Stepan <batykov@robotct.ru>
2016-10-03 11:01:18 +0500

    Fix CMakeLists.txt

--- make_debug.cmd
+++ make_debug.cmd
@@ -1,3 +1,3 @@
 cmake -E make_directory build
-cmake -E chdir build cmake .. -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_INSTALL_PREFIX=..
-cmake --build build --target install --clean-first
+cmake -E chdir build cmake .. -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=DEBUG
+cmake --build build
--- make_release.cmd
+++
@@ -1,3 +0,0 @@
-cmake -E make_directory build
-cmake -E chdir build cmake .. -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=..
-cmake --build build --target install --clean-first
